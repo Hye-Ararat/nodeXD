804bcc376a4fb188de938dd78a2bccce4c8b320a
Joseph Maldjian <josephavm@icloud.com>
2022-05-03 08:31:38 -0500

    Add Network Forward Fetching

--- src/network.ts
+++ src/network.ts
@@ -12,4 +12,9 @@ export default class Network {
             this.client.request.get(`/1.0/networks/${this.name}`).then(({data}) => resolve(data)).catch((err) => reject(err.response.data));
         })
     }
+    get forwards(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.client.request.get(`/1.0/networks/${this.name}/forwards?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response.data));
+        })
+    }
 }
