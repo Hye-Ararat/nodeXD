55c0a44a94a32a246ea81eec1bed3ffef930fb9d
Joseph Maldjian <josephavm@icloud.com>
2022-05-03 08:42:19 -0500

    Added network forward creation

--- src/network.ts
+++ src/network.ts
@@ -17,4 +17,14 @@ export default class Network {
             this.client.request.get(`/1.0/networks/${this.name}/forwards?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response.data));
         })
     }
+    createForward(description: String, listen_address: String, ports: Array<Object>, data?: Object): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.client.request.post(`/1.0/networks/${this.name}/forwards`, {
+                description: description,
+                listen_address: listen_address,
+                ports: ports,
+                ...data
+            }).then(({data}) => resolve(data)).catch((err) => reject(err.response.data));
+        })
+    }
 }
