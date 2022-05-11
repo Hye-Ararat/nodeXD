2f77605e4904f8320d151d2b75a41323abab40a7
Joseph Maldjian <josephavm@icloud.com>
2022-05-11 12:27:28 -0500

    add volume fetching

--- src/storage-pool.ts
+++ src/storage-pool.ts
@@ -12,4 +12,9 @@ export default class StoragePool {
             this.client.request.get(`/1.0/storage-pools/${this.name}`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
         })
     }
+    get volumes(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.client.request.get(`/1.0/storage-pools/${this.name}/volumes?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+        })
+    }
 }
