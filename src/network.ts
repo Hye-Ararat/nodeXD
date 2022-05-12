3970e12baa7628e096d6b950754228b7f904960c
Joseph Maldjian <josephavm@icloud.com>
2022-05-12 12:24:28 -0500

    add dhcp leases

--- src/network.ts
+++ src/network.ts
@@ -23,6 +23,11 @@ export default class Network {
             this.client.request.get(`/1.0/networks/${this.name}/forwards?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
         })
     }
+    get leases(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.client.request.get(`/1.0/networks/${this.name}/leases`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+        })
+    }
     createForward(description: String, listen_address: String, ports: Array<Object>, data?: Object): Promise<any> {
         return new Promise((resolve, reject) => {
             this.client.request.post(`/1.0/networks/${this.name}/forwards`, {
