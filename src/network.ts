7a95442a7cd4c44a51b2dfe72cf861c2b80811f6
Joseph Maldjian <josephavm@icloud.com>
2022-05-12 12:15:10 -0500

    add network state fetching

--- src/network.ts
+++ src/network.ts
@@ -13,6 +13,11 @@ export default class Network {
             this.client.request.get(`/1.0/networks/${this.name}`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
         })
     }
+    get state(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.client.request.get(`/1.0/networks/${this.name}/state`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+        })
+    }
     get forwards(): Promise<any> {
         return new Promise((resolve, reject) => {
             this.client.request.get(`/1.0/networks/${this.name}/forwards?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
