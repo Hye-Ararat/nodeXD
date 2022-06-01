cc0cf8910839e92b8929951742cb2e89b7f0dc46
Joseph Maldjian <josephavm@icloud.com>
2022-06-01 11:19:07 -0500

    add volume creation

--- src/storage-pool.ts
+++ src/storage-pool.ts
@@ -17,4 +17,12 @@ export default class StoragePool {
             this.client.request.get(`/1.0/storage-pools/${this.name}/volumes?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
         })
     }
+    createVolume(name: string, data?: Object): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.client.request.post(`/1.0/storage-pools/${this.name}/volumes`, {
+                name: name,
+                ...data
+            }).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+        })
+    }
 }
