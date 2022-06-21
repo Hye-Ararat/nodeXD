345f843e9f5c9b447dec11eed9c64a26f455f419
Joseph Maldjian <josephavm@icloud.com>
2022-06-21 09:53:38 -0500

    Add expanded data

--- src/storage-pool.ts
+++ src/storage-pool.ts
@@ -10,12 +10,17 @@ export default class StoragePool {
     }
     get data(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.client.request.get(`/1.0/storage-pools/${this.name}`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            this.client.request.get(`/1.0/storage-pools/${this.name}`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
+        })
+    }
+    get expandedData(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.client.request.get(`/1.0/storage-pools/${this.name}?recursion=1`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     get volumes(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.client.request.get(`/1.0/storage-pools/${this.name}/volumes?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            this.client.request.get(`/1.0/storage-pools/${this.name}/volumes?recursion=1`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     createVolume(name: string, data?: Object): Promise<any> {
@@ -23,7 +28,7 @@ export default class StoragePool {
             this.client.request.post(`/1.0/storage-pools/${this.name}/volumes`, {
                 name: name,
                 ...data
-            }).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            }).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     volume(name: string) {
