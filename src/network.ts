284b5c1862cbe90af1dc9548caff6d9b8f51b65a
Joseph Maldjian <josephavm@icloud.com>
2022-06-25 11:09:07 -0500

    Network Deletion

--- src/network.ts
+++ src/network.ts
@@ -10,27 +10,27 @@ export default class Network {
     }
     get data(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.client.request.get(`/1.0/networks/${this.name}`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            this.client.request.get(`/1.0/networks/${this.name}`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     get state(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.client.request.get(`/1.0/networks/${this.name}/state`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            this.client.request.get(`/1.0/networks/${this.name}/state`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     get forwards(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.client.request.get(`/1.0/networks/${this.name}/forwards?recursion=1`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            this.client.request.get(`/1.0/networks/${this.name}/forwards?recursion=1`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     get leases(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.client.request.get(`/1.0/networks/${this.name}/leases`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            this.client.request.get(`/1.0/networks/${this.name}/leases`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     partialUpdate(data: Object): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.client.request.patch(`/1.0/networks/${this.name}`, data).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            this.client.request.patch(`/1.0/networks/${this.name}`, data).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     createForward(description: String, listen_address: String, ports: Array<Object>, data?: Object): Promise<any> {
@@ -40,10 +40,15 @@ export default class Network {
                 listen_address: listen_address,
                 ports: ports,
                 ...data
-            }).then(({data}) => resolve(data)).catch((err) => reject(err.response));
+            }).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
         })
     }
     forward(listen_address: string) {
         return new Forward(listen_address, this, this.client);
+    }
+    delete() {
+        return new Promise((resolve, reject) => {
+            this.client.request.delete(`/1.0/networks/${this.name}`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
+        })
     }
 }
