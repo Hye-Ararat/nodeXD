d6f7f88ee3e2e3d836b7c700c34b71b709add4a4
Joseph Maldjian <josephavm@icloud.com>
2022-10-04 15:47:22 -0500

    Operations Websocket

--- src/operation.ts
+++ src/operation.ts
@@ -10,6 +10,15 @@ export default class Operation {
 
     get data(): Promise<any> {
         return new Promise((resolve, reject) => {
-             this.client.request.get(`/1.0/operations/${this.id}`).then(({data}) => resolve(data)).catch((err) => reject(err.response));
-        }) 
-     }}
+            this.client.request.get(`/1.0/operations/${this.id}`).then(({ data }) => resolve(data)).catch((err) => reject(err.response));
+        })
+    }
+    get websocket(): Promise<any> {
+        return new Promise(async (resolve, reject) => {
+            let operationData = await this.data;
+            let ws = this.client.request.websocket(operationData.metadata.fds["0"]);
+            resolve(ws);
+
+        })
+    }
+}
